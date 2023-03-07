cc3c0a25a4c083fe4cbc0f76d54770bff66b54f3
Inoriant <424504326@qq.com>
2023-03-08 01:25:52 +0800

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# nonebot_plugin_chatgpt_turbo_on_qq
-基于chatGPT-3.5-turboAPI的nonebot插件
--- config.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, Extra
-
-class Config(BaseModel, extra=Extra.ignore):
-    superUsers:set
